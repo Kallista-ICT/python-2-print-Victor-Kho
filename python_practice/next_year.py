b9299699a9f060e5d0b7ccf254fdb07a15300ca7
Victor-Kho <victorkho2009@gmail.com>
2025-01-22 01:40:06 +0000

    Submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,14 @@
+#This is to indicate that the code is running
+print("Hello, World!")
+
+#This code is used to ask for your name
+name= input('Enter your name: ')
+
+#This code is used to ask for your age
+age= int(input('Enter your age: '))
+
+#This code is used to greet you and tell how old you currently are
+print(f"Hello, {name}! You are {age} years old.")
+
+#This code is used to tell you that you will be older next year
+print(f"Next year you will be {age+1} years old.")
